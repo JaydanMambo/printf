5b3cec04a0eacec656fba7815465f3392dabedb9
JaydanMambo <jdanmambo@gmail.com>
2023-10-15 09:05:16 -0700

    Test

--- print_f_0.c
+++ print_f_0.c
@@ -12,43 +12,56 @@
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
-va_start(args, format);
-while (*format != '\0')
-{
-if (*format != '%')
-{
-write(1, format, 1); /* Write a single character to stdout */
-count++;
-}
-else
-{
-format++; /* Move past the '%'sign */
-if (*format == 'c')
-{
-char c = va_arg(args, int);
-write(1, &c, 1);
-count++;
-}
-else if (*format == 's')
-{
-char *str = va_arg(args, char *);
-while (*str != '\0')
-{
-write(1, str, 1);
-str++;
-count++;
-}
-}
-else if (*format == '%')
-{
-write(1, "%", 1);
-count++;
-}
-}
-format++;
-}
-va_end(args);
-return (count);
+	va_list args;
+    va_start(args, format);
+
+    int count = 0;  // Initialize the character count.
+
+    while (*format != '\0') {
+        if (*format != '%') {
+            write(1, format, 1);  // Write a single character to stdout
+            count++;
+        } else {
+            format++; // Move past the '%'
+            switch (*format) {
+                case 'c': {
+                    int c = va_arg(args, int);
+                    write(1, &c, 1);  // Write the character to stdout
+                    count++;
+                    break;
+                }
+                case 's': {
+                    char *str = va_arg(args, char *);
+                    while (*str != '\0') {
+                        write(1, str, 1);  // Write a character at a time
+                        str++;
+                        count++;
+                    }
+                    break;
+                }
+                case 'd':
+                case 'i': {
+                    int num = va_arg(args, int);
+                    char num_str[12];  // Maximum integer digits + sign
+                    int num_len = snprintf(num_str, sizeof(num_str), "%d", num);
+                    write(1, num_str, num_len);
+                    count += num_len;
+                    break;
+                }
+                case '%':
+                    write(1, "%", 1);  // Write '%' character
+                    count++;
+                    break;
+                default:
+                    write(1, "%", 1);  // Write '%' character
+                    write(1, format, 1);  // Write the unsupported specifier
+                    count += 2;
+                    break;
+            }
+        }
+        format++;
+    }
+
+    va_end(args);
+    return (count);
 }
