0a4612270d05528b0c72d64604ea675d5279762d
JaydanMambo <jdanmambo@gmail.com>
2023-10-15 08:54:55 -0700

    Remove spaces

--- print_f_0.c
+++ print_f_0.c
@@ -12,47 +12,43 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
-
-	va_start(args, format);
-
-	while (*format != '\0')
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1); /* Write a single character to stdout */
-			count++;
-		}
-		else
-		{
-			format++; /* Move past the '%'sign */
-			if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-
-				write(1, &c, 1);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char *);
-
-				while (*str != '\0')
-				{
-					write(1, str, 1);
-					str++;
-					count++;
-				}
-			}
-			else if (*format == '%')
-			{
-				write(1, "%", 1);
-				count++;
-			}
-		}
-		format++;
-	}
-	va_end(args);
-	return (count);
+va_list args;
+int count = 0;
+va_start(args, format);
+while (*format != '\0')
+{
+if (*format != '%')
+{
+write(1, format, 1); /* Write a single character to stdout */
+count++;
+}
+else
+{
+format++; /* Move past the '%'sign */
+if (*format == 'c')
+{
+char c = va_arg(args, int);
+write(1, &c, 1);
+count++;
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+while (*str != '\0')
+{
+write(1, str, 1);
+str++;
+count++;
+}
+}
+else if (*format == '%')
+{
+write(1, "%", 1);
+count++;
+}
+}
+format++;
+}
+va_end(args);
+return (count);
 }
