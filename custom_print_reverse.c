a16299211c5176febfd6d00a4b7e942c436e5fb9
JaydanMambo <jdanmambo@gmail.com>
2023-10-17 03:21:29 -0700

    Print In Reverse

---
+++ custom_print_reverse.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include "main.h"
+/**
+ * custom_print_reverse - Prints a reversed string
+ * @str: The input string to be reversed and printed
+ *
+ * Return: The number of characters printed
+ */
+int custom_print_reverse(const char *str)
+{
+	int count = 0;
+
+	if (str)
+	{
+		const char *end = str;
+		while (*end)
+		{
+			 end++;
+		}
+		end--;
+		while (end >= str)
+		{
+			our_putchar(*end);
+			end--;
+			count++;
+		}
+	}
+	return (count);
+}
--- main.h
+++ main.h
@@ -13,4 +13,6 @@ int custom_print_octal(unsigned int num);
 int custom_print_hex(unsigned int num, char base_char);
 int custom_print_S(const char *str);
 int custom_print_ptr(void *ptr);
+int custom_print_reverse(const char *str);
+
 #endif
